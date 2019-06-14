2ff9803400d8770e1d45663990e009804ea51d7c
MiladRouygari <miladrouygari@gmail.com>
2019-06-14 20:01:57 +0200

    Update 05_views.js

--- ExperimentsFiles/05_views.js
+++ ExperimentsFiles/05_views.js
@@ -23,7 +23,7 @@ const intro = babeViews.view_generator("intro",{
     trials: 1,
     name: 'intro',
     // If you use JavaScripts Template String `I am a Template String`, you can use HTML <></> and javascript ${} inside
-    text:   `This is a sample .......introduction view.
+    text:   `This is a sample .......introduction view. Duh Dah Beh
             <br />
             <br />
             The introduction view welcomes the participant and gives general information
