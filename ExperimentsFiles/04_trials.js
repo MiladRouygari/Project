0971bf717f3f1875aa6ee95ba13addc70c74d76f
lewalt <leo.waltert@web.de>
2019-06-19 16:23:10 +0200

    first ideas for implemeting  english/german trials

--- ExperimentsFiles/04_trials.js
+++ ExperimentsFiles/04_trials.js
@@ -3,25 +3,57 @@
 //This might need to be changed, if we want to counter balanced between Decision and imagination,
 //I don't think imagination needs to be counter balanced or does it?
 
-function get_language(){
+// this function returns random languege english/german
+function getLanguage(){
     return (Math.floor(Math.random()*2) == 0) ? 'german': 'english';
 }
 
-const trial_info = {
-    forced_choice: [
-        {
-            question: "What's on the bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
-        },
-        {
-            question: "What's the weather like?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
-        }
-    ]
-};
+language = getLanguage()
+exp = {}
+
+// german trials
+if(language == 'german'){
+    exp = {
+        forced_choice: [
+            {
+                question: "Was ist auf dem Brot?",
+                picture: "images/question_mark_02.png",
+                option1: 'jam',
+                option2: 'ham',
+                correct: 'jam'
+            },
+            {
+                question: "Wie sieht das Wetter aus?",
+                picture: "images/weather.jpg",
+                option1: "shiny",
+                option2: "rainbow",
+                correct: "shiny"
+            }
+        ]
+    };
+}
+
+// english trials
+else{
+    exp = {
+        forced_choice: [
+            {
+                question: "What's on the bread?",
+                picture: "images/question_mark_02.png",
+                option1: 'jam',
+                option2: 'ham',
+                correct: 'jam'
+            },
+            {
+                question: "What's the weather like?",
+                picture: "images/weather.jpg",
+                option1: "shiny",
+                option2: "rainbow",
+                correct: "shiny"
+            }
+        ]
+    };
+}
+
+
+const trial_info = exp
