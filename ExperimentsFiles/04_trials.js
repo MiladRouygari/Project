5a11f10cfde7ad6bef3b28b4e6bcccef50dd0967
Milad Rouygari <miladrouygari@gmail.com>
2019-06-22 16:54:27 +0200

    Descriptions as Leona Wrote

--- ExperimentsFiles/04_trials.js
+++ ExperimentsFiles/04_trials.js
@@ -7,109 +7,101 @@
 const trial_info = {
     forced_choice_1_german: [
         {
-            question: "Was ist auf 'D00a' dem ersten Brot?",
+            question: "Würdest du 'D00a' den Mann stoßen?",
             picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+            optionLeft: 'definitiv stoßen',
+            optionRight: 'definitiv nicht stoßen',
         },
         {
-              question: "Wie ist 'D11a' das Wetter?",
-              picture: "images/weather.jpg",
-              option1: "shiny",
-              option2: "rainbow",
-              correct: "shiny"
+            question: "Wie lebhaft 'D11a' stellst du dir den Mann vor?",
+            picture: "images/weather.jpg",
+            optionLeft: "absolut klar",
+            optionRight: "kein Bild",
         },
         {
-              question: "Wie ist 'D22a' das Wetter?",
-              picture: "images/weather.jpg",
-              option1: "shiny",
-              option2: "rainbow",
-              correct: "shiny"
+            question: "Wie lebhaft 'D22a' stellst du dir die fünf Menschen vor?",
+            picture: "images/weather.jpg",
+            optionLeft: "absolut klar",
+            optionRight: "kein Bild",
         },
         {
-              question: "Wie ist 'D33a' das Wetter?",
-              picture: "images/weather.jpg",
-              option1: "shiny",
-              option2: "rainbow",
-              correct: "shiny"
+            question: "Wie lebhaft 'D33a' stellst du dir die gesamte Situation vor?",
+            picture: "images/weather.jpg",
+            optionLeft: "absolut klar",
+            optionRight: "kein Bild",
         }
     ],
     forced_choice_2_german: [
       {
-          question: "Was ist auf 'D00b' dem ersten Brot?",
-          picture: "images/question_mark_02.png",
-          option1: 'jam',
-          option2: 'ham',
-          correct: 'jam'
+            question: "Was ist auf 'D00b' dem ersten Brot?",
+            picture: "images/question_mark_02.png",
+            option1: 'jam',
+            option2: 'ham',
+            correct: 'jam'
       },
       {
-          question: "Wie ist 'D11b' das Wetter?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "Wie ist 'D11b' das Wetter?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "Wie ist 'D22b' das Wetter?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "Wie ist 'D22b' das Wetter?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "Wie ist 'D33b' das Wetter?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "Wie ist 'D33b' das Wetter?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       }
     ],
     forced_choice_3_german: [
       {
-          question: "Was ist auf 'D00c' dem ersten Brot?",
-          picture: "images/question_mark_02.png",
-          option1: 'jam',
-          option2: 'ham',
-          correct: 'jam'
+            question: "Was ist auf 'D00c' dem ersten Brot?",
+            picture: "images/question_mark_02.png",
+            option1: 'jam',
+            option2: 'ham',
+            correct: 'jam'
       },
       {
-          question: "Wie ist 'D11c' das Wetter?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "Wie ist 'D11c' das Wetter?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "Wie ist D'22c' das Wetter?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "Wie ist D'22c' das Wetter?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "Wie ist 'D33c' das Wetter?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "Wie ist 'D33c' das Wetter?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       }
 
     ],
     forced_choice_1_english: [
         {
-            question: "What's on 'E00a' the first bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+            question: "Would you 'E00a' push the man?",
+            optionLeft: 'definitely would not push him',
+            optionRight: 'definitely would push him'
         },
         {
-            question: "How is 'E11a' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+            question: "How vividly 'E11a'can you picture this scene?",
+            optionLeft: "no image",
+            optionRight: "very clear"
         },
         {
             question: "How is 'E22a' the weather?",
@@ -128,62 +120,62 @@ const trial_info = {
     ],
     forced_choice_2_english: [
       {
-          question: "What's on 'E00b' the first bread?",
-          picture: "images/question_mark_02.png",
-          option1: 'jam',
-          option2: 'ham',
-          correct: 'jam'
+            question: "What's on 'E00b' the first bread?",
+            picture: "images/question_mark_02.png",
+            option1: 'jam',
+            option2: 'ham',
+            correct: 'jam'
       },
       {
-          question: "How is 'E11b' the weather?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "How is 'E11b' the weather?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "How is 'E22b' the weather?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "How is 'E22b' the weather?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "How is 'E33b' the weather?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "How is 'E33b' the weather?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       }
     ],
     forced_choice_3_english: [
       {
-          question: "What's on 'E00c' the first bread?",
-          picture: "images/question_mark_02.png",
-          option1: 'jam',
-          option2: 'ham',
-          correct: 'jam'
+            question: "What's on 'E00c' the first bread?",
+            picture: "images/question_mark_02.png",
+            option1: 'jam',
+            option2: 'ham',
+            correct: 'jam'
       },
       {
-          question: "How is 'E11c' the weather?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "How is 'E11c' the weather?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "How is 'E22c' the weather?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "How is 'E22c' the weather?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       },
       {
-          question: "How is 'E33c' the weather?",
-          picture: "images/weather.jpg",
-          option1: "shiny",
-          option2: "rainbow",
-          correct: "shiny"
+            question: "How is 'E33c' the weather?",
+            picture: "images/weather.jpg",
+            option1: "shiny",
+            option2: "rainbow",
+            correct: "shiny"
       }
     ],
 };
