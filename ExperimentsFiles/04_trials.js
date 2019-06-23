a9b7ca9d47498087897c5ccc9765fcd384f2f0f6
lewalt <leo.waltert@web.de>
2019-06-23 12:32:39 +0200

    edited trial info data

--- ExperimentsFiles/04_trials.js
+++ ExperimentsFiles/04_trials.js
@@ -7,175 +7,134 @@
 const trial_info = {
     forced_choice_1_german: [
         {
-            question: "Würdest du 'D00a' den Mann stoßen?",
-            picture: "images/question_mark_02.png",
+            question: "Würdest du den Mann stoßen?",
             optionLeft: 'definitiv stoßen',
             optionRight: 'definitiv nicht stoßen',
         },
         {
-            question: "Wie lebhaft 'D11a' stellst du dir den Mann vor?",
-            picture: "images/weather.jpg",
+            question: "Wie lebhaft stellst du dir den Mann vor?",
             optionLeft: "absolut klar",
             optionRight: "kein Bild",
         },
         {
-            question: "Wie lebhaft 'D22a' stellst du dir die fünf Menschen vor?",
-            picture: "images/weather.jpg",
+            question: "Wie lebhaft stellst du dir die fünf Menschen vor?",
             optionLeft: "absolut klar",
             optionRight: "kein Bild",
         },
         {
-            question: "Wie lebhaft 'D33a' stellst du dir die gesamte Situation vor?",
-            picture: "images/weather.jpg",
+            question: "Wie lebhaft stellst du dir die gesamte Situation vor?",
             optionLeft: "absolut klar",
             optionRight: "kein Bild",
         }
     ],
     forced_choice_2_german: [
       {
-            question: "Was ist auf 'D00b' dem ersten Brot?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+          question: "Würdest du den Mann stoßen ? -need to edit here-",
+          optionLeft: 'definitiv stoßen',
+          optionRight: 'definitiv nicht stoßen',
       },
       {
-            question: "Wie ist 'D11b' das Wetter?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Wie lebhaft stellst du dir den Mann vor?",
+          optionLeft: "absolut klar",
+          optionRight: "kein Bild",
       },
       {
-            question: "Wie ist 'D22b' das Wetter?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Wie lebhaft stellst du dir die fünf Menschen vor?",
+          optionLeft: "absolut klar",
+          optionRight: "kein Bild",
       },
       {
-            question: "Wie ist 'D33b' das Wetter?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Wie lebhaft stellst du dir die gesamte Situation vor?",
+          optionLeft: "absolut klar",
+          optionRight: "kein Bild",
       }
     ],
     forced_choice_3_german: [
       {
-            question: "Was ist auf 'D00c' dem ersten Brot?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+          question: "Würdest du den Mann stoßen ? -need to edit here-",
+          optionLeft: 'definitiv stoßen',
+          optionRight: 'definitiv nicht stoßen',
       },
       {
-            question: "Wie ist 'D11c' das Wetter?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Wie lebhaft stellst du dir den Mann vor?",
+          optionLeft: "absolut klar",
+          optionRight: "kein Bild",
       },
       {
-            question: "Wie ist D'22c' das Wetter?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Wie lebhaft stellst du dir die fünf Menschen vor?",
+          optionLeft: "absolut klar",
+          optionRight: "kein Bild",
       },
       {
-            question: "Wie ist 'D33c' das Wetter?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Wie lebhaft stellst du dir die gesamte Situation vor?",
+          optionLeft: "absolut klar",
+          optionRight: "kein Bild",
       }
-
     ],
     forced_choice_1_english: [
         {
-            question: "Would you 'E00a' push the man?",
+            question: "Would you push the man?",
             optionLeft: 'definitely would not push him',
             optionRight: 'definitely would push him'
         },
         {
-            question: "How vividly 'E11a'can you picture this scene?",
+            question: "How vividly can you picture the man?",
             optionLeft: "no image",
             optionRight: "very clear"
         },
         {
-            question: "How is 'E22a' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+            question: "How vividly can you picture the five people?",
+            optionLeft: "no image",
+            optionRight: "very clear"
         },
         {
-            question: "How is 'E33a' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
-        }
+            question: "How vividly can you picture the overall scene?",
+            optionLeft: "no image",
+            optionRight: "very clear"
+        },
     ],
     forced_choice_2_english: [
       {
-            question: "What's on 'E00b' the first bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+          question: "Would you push the man?",
+          optionLeft: 'definitely would not push him',
+          optionRight: 'definitely would push him'
       },
       {
-            question: "How is 'E11b' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "How vividly can you picture the man?",
+          optionLeft: "no image",
+          optionRight: "very clear"
       },
       {
-            question: "How is 'E22b' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "How vividly can you picture the five people?",
+          optionLeft: "no image",
+          optionRight: "very clear"
       },
       {
-            question: "How is 'E33b' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
-      }
-    ],
+          question: "How vividly can you picture the overall scene?",
+          optionLeft: "no image",
+          optionRight: "very clear"
+      },
+  ],
     forced_choice_3_english: [
       {
-            question: "What's on 'E00c' the first bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+          question: "Would you push the man?",
+          optionLeft: 'definitely would not push him',
+          optionRight: 'definitely would push him'
       },
       {
-            question: "How is 'E11c' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "How vividly can you picture the man?",
+          optionLeft: "no image",
+          optionRight: "very clear"
       },
       {
-            question: "How is 'E22c' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "How vividly can you picture the five people?",
+          optionLeft: "no image",
+          optionRight: "very clear"
       },
       {
-            question: "How is 'E33c' the weather?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
-      }
-    ],
+          question: "How vividly can you picture the overall scene?",
+          optionLeft: "no image",
+          optionRight: "very clear"
+      },
+  ],
 };
